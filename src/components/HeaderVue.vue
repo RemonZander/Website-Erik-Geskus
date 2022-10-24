0911786253887c9352d19fa54c5a3a2038fb637d
Bas1159 <baskruithof85@outlook.com>
2022-10-24 13:50:07 +0200

    Show burger menu button on smaller screens

--- src/components/HeaderVue.vue
+++ src/components/HeaderVue.vue
@@ -7,7 +7,7 @@
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
 
-        <nav v-if="hidden === false" :class="{stacked: hidden === true}" class="flex m-auto py-3 opacity-70">
+        <nav v-if="shown === false" v-bind:class="{stacked: shown === true}" class="flex m-auto py-3 opacity-70">
           <ul>
             <li>
               <a href="#">Biografie</a>
@@ -35,12 +35,12 @@ export default {
   name: "HeaderVue",
   methods: {
     hideMenu() {
-      this.hidden = this.hidden === false;
+      this.shown = this.shown === false;
     }
   },
   data() {
     return {
-      hidden: false
+      shown: false
     }
   }
 }
@@ -50,5 +50,17 @@ export default {
   .stacked {
     display: flex;
     flex-direction: column;
+  }
+
+  @media only screen and (max-width: 414px) {
+    .burgerMenu {
+      display: flex;
+    }
+  }
+
+  @media only screen and (min-width: 600px) {
+    .burgerMenu {
+      display: none;
+    }
   }
 </style>
