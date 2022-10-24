ff64f0f7f27fa26c3f162c41c181fc52577d3353
Bas1159 <baskruithof85@outlook.com>
2022-10-24 14:55:11 +0200

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="flex bg-black text-white justify-center">
-    Footer
+  <div class="bg-black text-white">
+    <div class="flex justify-center place-items-center">
+      <form class="bg-gray-50">
+        <label for="email">Email</label>
+        <input type="text" id="email">
+      </form>
+    </div>
+    <div>
+    </div>
   </div>
 </template>
 
