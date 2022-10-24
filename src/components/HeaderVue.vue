c429337918bef1a4ad7d217b14d83346604b306a
niek221 <116267865+niek221@users.noreply.github.com>
2022-10-24 14:54:57 +0200

    Update HeaderVue.vue

--- src/components/HeaderVue.vue
+++ src/components/HeaderVue.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-col bg-black text-white">
 
-        <h1 class="flex m-auto text-5xl py-6">Eric Geskus</h1>
+        <h1 class="flex m-auto text-5xl py-6">Erik Geskus</h1>
 
-        <nav class="flex m-auto py-3 opacity-70 sm:hidden">
-          <ul>
+        <nav class="flex m-auto py-3 opacity-70">
+          <ul class="flex flex-row gap-3">
             <li>
               <a href="#">Biografie</a>
             </li>
