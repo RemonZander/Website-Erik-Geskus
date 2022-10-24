a26fc5ee3e1ecfd90f4e7d6ed4b96877147b8868
Bas1159 <baskruithof85@outlook.com>
2022-10-24 13:30:09 +0200

    Added show / hide functionality

--- src/components/HeaderVue.vue
+++ src/components/HeaderVue.vue
@@ -3,11 +3,11 @@
 
         <h1 class="flex m-auto text-5xl py-6">Eric Geskus</h1>
 
-        <svg xmlns="http://www.w3.org/2000/svg" class="m-auto w-12 h-12 md:hidden " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg @click="hideMenu" xmlns="http://www.w3.org/2000/svg" class="m-auto w-12 h-12 md:hidden " fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
 
-        <nav class="flex m-auto py-3 opacity-70">
+        <nav v-if="hidden === false" :class="{stacked: hidden === true}" class="flex m-auto py-3 opacity-70">
           <ul>
             <li>
               <a href="#">Biografie</a>
@@ -33,19 +33,22 @@
 <script>
 export default {
   name: "HeaderVue",
+  methods: {
+    hideMenu() {
+      this.hidden = this.hidden === false;
+    }
+  },
+  data() {
+    return {
+      hidden: false
+    }
+  }
 }
 </script>
 
 <style scoped>
-  ul {
+  .stacked {
     display: flex;
-    flex-direction: row;
-    gap: 20px;
+    flex-direction: column;
   }
-
-  li {
-    color: #fff;
-  }
-
-
 </style>
