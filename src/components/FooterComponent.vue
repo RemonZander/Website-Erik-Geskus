6a6243dcc86c8ffc91e91dcd31c70e45b2f281f2
Bas1159 <baskruithof85@outlook.com>
2022-10-24 18:09:37 +0200

    Started footer design

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="flex justify-center">
+      <p class="text-white text-5xl pt-3 opacity-50">Contact opnemen</p>
+    </div>
+    <div>
+      <div class="red-line mb-10"></div>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent"
+}
+</script>
+
+<style scoped>
+  .red-line {
+    height: 5px;
+    background: darkred;
+    width: 61.1%;
+  }
+</style>
