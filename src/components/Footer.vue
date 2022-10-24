de5b544e59d3ef6330c26abbb0c36124e08a0ac5
Bas1159 <baskruithof85@outlook.com>
2022-10-24 10:23:38 +0200

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex bg-black text-white justify-center">
+    Footer
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
