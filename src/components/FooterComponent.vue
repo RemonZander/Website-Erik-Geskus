a192253ff0795fd828a79517e9a3464d3bcc7e8d
Bas1159 <baskruithof85@outlook.com>
2022-10-27 11:54:52 +0200

    Commit footer for Niek

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,57 +1,63 @@
 <template>
   <div>
-    <div class="flex justify-center">
-      <p class="text-white text-5xl pt-96 opacity-50">Contact opnemen</p>
+    <div>
+      <p class="text-white text-5xl pt-[20%] pl-[47%] opacity-50">Contact opnemen</p>
     </div>
     <div>
-      <div class="red-line mb-10"></div>
+      <div class="h-2 bg-red-800 w-8/12 mb-[2%]"></div>
     </div>
-    <div class="flex pl-96 flex-col text-elipsis">
+    <div class="flex pl-[20%] flex-col text-elipsis">
       <div class="text-2xl text-white mb-10">Gëinspireerd geraakt?</div>
     </div>
     <div class="flex flex-col text-clip overflow-hidden">
-      <div class="pl-96 text-white opacity-50 ">
+      <div class="pl-[20%] text-white opacity-50">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer tos It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </div>
     </div>
-    <div class="flex justify-center text-white py-6">
-      <div class="pb-6">
+    <div class="flex flex-col pb-[1%]">
+      <div class="pt-[3%] pl-[20%] pb-6 text-white text-2xl">Erik Geskus</div>
+    </div>
+    <div>
+      <div class="flex flex-row">
+        <Icon class="text-white pl-5" icon="mdi-light:phone"></Icon>
+        <div class="pl-[20%] pb-[0.5%] text-white opacity-50">06 12 34 58 78</div>
+      </div>
+    </div>
+    <div class="flex flex-row">
+      <Icon icon="mdi-light:email"></Icon>
+      <div class="pl-[20%] pb-[0.5%] text-white opacity-50">info@erikgeskus.nl</div>
+    </div>
+    <div class="flex flex-row">
+      <Icon icon="mdi-light:youtube"></Icon>
+      <div class="pl-[20%] pb-[0.5%] text-white opacity-50">Erik Geskus</div>
+    </div>
+    <div class="pl-[20%] pb-[5%] pt-[2%]">
+      <div class="text-white text-black pb-[3%] pt-[3%] flex flex-col bg-white opacity-30 w-[50%] h-50 place-items-center justify-center rounded-lg">
+        <div class="text-2xl pb-[3%]">Contact formulier</div>
         <form>
-          <div class="flex justify-center flex-col mb-6 bg-orange-800 w-100 rounded-lg">
-            <h1 class="font-bold pb-5">Contact formulier</h1>
-            <div>
-              <label for="naam" class="block mb-2 text-sm font-medium text-gray dark:text-white">Voor & Achternaam</label>
-              <input type="text" id="naam" class="shadow-sm bg-black border border-orange-800 text-white text-sm rounded-lg">
-            </div>
-            <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray dark:text-white">Email</label>
-              <input type="email" id="email" class="shadow-sm bg-black border border-orange-800 text-white text-sm rounded-lg">
-            </div>
-            <div>
-              <label  for="phoneNumber" class="block mb-2 text-sm font-medium text-gray dark:text-white">Telefoon nummer</label>
-              <input type="text" id="phoneNumber" class="shadow-sm bg-black border border-orange-800 text-white text-sm rounded-lg">
-            </div>
-            <div class="">
-              <button @click.prevent="submitForm"  class="bg-green-500 ml-9 rounded-lg mb-3 text-white mt-5 text-center">
-                Verzenden
-              </button>
-            </div>
+          <div class="text-white flex flex-col pb-[2%%]">
+            <label for="naam" class="text-sm text-black">Uw naam</label>
+            <input type="text" id="naan" class="rounded-lg bg-black w-full dark:text-white">
+          </div>
+          <div class="text-white flex flex-col pb-[2%]">
+            <label for="email" class="text-sm text-black">Uw email</label>
+            <input type="text" id="email" class="rounded-lg bg-black w-full dark:text-white">
+          </div>
+          <div class="text-white flex flex-col pb-[2%] mt-[4%]">
+            <button class="rounded-lg bg-red-500 text- pl-[2%]">Contactverzoek opsturen</button>
           </div>
         </form>
       </div>
-    </div>
-    <div class="flex flex-col pb-8">
-      <div class="pt-12 pl-96 pb-6 text-white text-2xl">Erik Geskus</div>
-      <div class="pl-96 pb-2 text-white opacity-50">06 12 34 58 78</div>
-      <div class="pl-96 pb-2 text-white opacity-50">info@erikgeskus.nl</div>
-      <div class="pl-96 pb-12 text-white opacity-50">Erik Geskus</div>
     </div>
   </div>
 </template>
 
 <script>
+import {Icon} from "@iconify/vue";
+
 export default {
   name: "FooterComponent",
+  components: {Icon},
   methods: {
     submitForm() {
       /**
