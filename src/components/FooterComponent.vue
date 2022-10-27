1b30184d8d277ddeb9976dd40757ca78e8994d32
niek221 <116267865+niek221@users.noreply.github.com>
2022-10-27 14:01:02 +0200

    Update FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <div>
-      <p class="text-white text-5xl pt-[20%] pl-[47%]">Contact opnemen</p>
-    </div>
-    <div>
-      <div class="h-2 bg-red-800 w-8/12 mb-[2%]"></div>
+    <div class="flex flex-col w-6/12 mb-[2%] pt-[20%]">
+      <p class="text-white text-3xl self-end lg:text-5xl">Contact opnemen</p>
+      <div class="h-2 bg-red-800"></div>
     </div>
     <div class="flex pl-[20%] flex-col text-elipsis">
       <div class="text-2xl text-white mb-10">Gëinspireerd geraakt?</div>
@@ -18,36 +16,36 @@
       <div class="pt-[3%] pl-[20%] pb-6 text-white text-2xl">Erik Geskus</div>
     </div>
     <div>
-      <div class="flex flex-row">
+      <div class="flex flex-row items-center gap-2 pl-[20%]">
         <Icon class="text-white pl-5" icon="mdi-light:phone"></Icon>
-        <div class="pl-[20%] pb-[0.5%] text-white opacity-50">06 12 34 58 78</div>
+        <div class=" text-white opacity-50">06 12 34 58 78</div>
       </div>
     </div>
-    <div class="flex flex-row">
-      <Icon icon="mdi-light:email"></Icon>
-      <div class="pl-[20%] pb-[0.5%] text-white opacity-50">info@erikgeskus.nl</div>
+    <div class="flex flex-row items-center gap-2 pl-[20%]">
+      <Icon class="text-white" icon="mdi-light:email"></Icon>
+      <div class=" text-white opacity-50">info@erikgeskus.nl</div>
     </div>
-    <div class="flex flex-row">
-      <Icon icon="mdi-light:youtube"></Icon>
-      <div class="pl-[20%] pb-[0.5%] text-white opacity-50">Erik Geskus</div>
+    <div class="flex flex-row items-center gap-2 pl-[20%]">
+      <Icon class="text-white" icon="mdi-light:youtube"></Icon>
+      <div class=" text-white opacity-50">Erik Geskus</div>
     </div>
     <div class="pl-[20%] pb-[5%] pt-[2%]">
-      <div class="text-white text-black pb-[3%] pt-[3%] flex flex-col bg-white w-[50%] h-50 place-items-center justify-center rounded-lg">
+      <div class="text-white pb-[3%] pt-[3%] flex flex-col bg-black w-[15%] place-items-center justify-center rounded-lg">
         <div class="text-2xl pb-[3%]">Contact formulier</div>
-        <form>
-          <div class="flex flex-col pb-[2%%]">
-            <label for="naam" class="text-sm text-black">Uw naam</label>
-            <input type="text" id="naan" class="rounded-lg border-2 bg-white hover:border-red-800 w-full dark:text-white ">
+        <form class="">
+          <div class="flex flex-col pb-[2%]">
+            <label for="naam" class="text-sm text-white">Uw naam</label>
+            <input type="text" id="naam" class="rounded-lg border-2 bg-white hover:border-red-800 w-full text-black">
           </div>
 
           <div class="flex flex-col pb-[2%]">
-            <label for="email" class="text-sm text-black">Uw email</label>
-            <input type="text" id="email" class="rounded-lg border-2 focus:border-red-800 bg-white w-full dark:text-white">
+            <label for="naam" class="text-sm text-white">Uw email</label>
+            <input type="text" id="email" class="rounded-lg border-2 bg-white hover:border-red-800 w-full text-black">
           </div>
 
           <div class="flex flex-col pb-[2%]">
-            <label for="email" class="text-sm text-black">Uw vraag</label>
-            <input type="text" id="email" class="rounded-lg border-2 bg-white w-full dark:text-white">
+            <label for="vraag" class="text-sm text-white">Uw vraag</label>
+            <textarea id="vraag" class="rounded-lg border-2 bg-white hover:border-red-800 w-full text-black"> </textarea>
           </div>
 
           <div class="flex flex-col pb-[2%] mt-[4%]">
