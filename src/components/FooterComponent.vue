7cbc98a90956dc443f692347c1d5617ad9baf749
Bas1159 <baskruithof85@outlook.com>
2022-11-07 14:17:07 +0100

    Fixed footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,17 +1,17 @@
 <template xmlns:form="http://www.w3.org/1999/html">
   <div>
-    <div class="flex flex-col w-6/12 mb-[2%] pt-[20%]">
+    <div class="flex flex-col w-6/12 mb-7 pt-[20%]">
       <p class="text-white self-end sm:text-1xl md:text-3xl lg:text-5xl">Contact opnemen</p>
       <div class="h-2 bg-red-800"></div>
     </div>
     <div class="flex flex-col text-clip overflow-hidden ">
-      <div class="flex flex-col text-elipsis mt-[2%] pl-[20%]">
+      <div class="flex flex-col text-elipsis mt-5 pl-[20%]">
         <div class="flex text-1xl lg:text-2xl text-white mb-10">Gëinspireerd geraakt?</div>
       </div>
       <div class="pl-[20%] pr-[20%] text-white opacity-50">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer tos It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </div>b-[1%]">
-      <div class="pt-[3%] pl-[20%] pb-6 text-white text-2xl">Erik Geskus</div>
+      </div>
+      <div class="pt-7 pl-[20%] pb-6 text-white text-2xl">Erik Geskus</div>
     </div>
     <div>
       <div class="flex flex-row items-center gap-2 pl-[20%]">
@@ -29,20 +29,21 @@
     </div>
     <div>
       <form class="">
-        <div class="flex flex-col pb-[0.5%] pt-[0.5%] pl-[20%]">
-          <label for="naam" class="text-sm text-white">Uw naam</label>
-          <input v-model="nameValue" type="text" id="naam" class="w-1/3 rounded-lg input-width-30" placeholder="Voornaam">
+        <div class="flex flex-col pt-10 pl-[20%]">
+          <div class="flex text-2xl text-white mb-10">Contact Formulier</div>
+          <label for="naam" class="text-sm text-white pb-2">Uw naam</label>
+          <input v-model="nameValue" type="text" id="naam" class="w-2/3 sm:w-1/3 rounded-lg text-black" placeholder="Voornaam">
         </div>
-        <div class="flex flex-col pb=[0.5%] pt-[0.5%] pl-[20%]">
-          <label for="email" class="text-sm text-white">UW email</label>
-          <input v-model="emailValue" type="text" id="email" class="w-1/3 rounded-lg input-width-30" placeholder="voorbeeld@gmail.com">
+        <div class="flex flex-col pt-3 pl-[20%]">
+          <label for="email" class="text-sm text-white pb-2">Uw email</label>
+          <input v-model="emailValue" type="text" id="email" class="w-2/3 sm:w-1/3 rounded-lg" placeholder="voorbeeld@gmail.com">
         </div>
-        <div class="flex flex-col pb-[0.5%] pt-[0.5%] pl-[20%]">
-          <label for="vraag" class="text-sm text-white">Uw vraag</label>
-          <textarea v-model="questionValue" rows="10" id="vraag" class="w-1/3 rounded-lg input-width-30" placeholder="Ik heb een vraag"></textarea>
+        <div class="flex flex-col pt-3 pl-[20%]">
+          <label for="vraag" class="text-sm text-white pb-2">Uw vraag</label>
+          <textarea v-model="questionValue" rows="10" id="vraag" class="w-2/3 sm:w-1/3 rounded-lg" placeholder="Ik heb een vraag"></textarea>
         </div>
-        <div class="flex flex-col pb-[0.5%] pt-[0.5%] pl-[20%]">
-          <button @click.prevent="submitForm" class="w-1/3 rounded-lg bg-green-500 text-white button-width-30">Stuur contactverzoek</button>
+        <div class="flex flex-col pt-[1%] pb-[1%] pl-[20%]">
+          <button @click.prevent="submitForm" class="w-2/3 sm:w-1/3 rounded-lg bg-red-800 hover:bg-red-600 text-white ">Stuur contactverzoek</button>
         </div>
       </form>
     </div>
@@ -52,7 +53,6 @@
 <script>
 import {Icon} from "@iconify/vue";
 import axios from 'axios'
-
 export default {
   name: "FooterComponent",
   components: {Icon},
