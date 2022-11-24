92656da5e07ff9f56cebce0d7ccbb0f24045deea
niek221 <116267865+niek221@users.noreply.github.com>
2022-11-24 12:59:57 +0100

    News branch

---
+++ src/components/NewsComponent.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div class="w-1/2 float-right">
+
+      <div class="width-[40%] align-right mb-[10%] pt-[10%]">
+
+        <p class="text-white text-3xl align-items-end">Nieuwsoverzicht</p>!
+        <div class="h-2 bg-red-800"></div>
+
+      </div>
+
+      <div class="float-right w-0"></div>
+        <div class="float-right clear-right">
+          <iframe class="float-right" src="https://www.youtube.com/embed/tgbNymZ7vqY" width="420" height="315"></iframe>
+        </div>
+
+        <h2 class="text-xl" id="title">Recenste Youtube Post</h2>
+        <p class="text-white pt-2 opacity-50" id="date">19 Maart 2022</p>
+        <p class="text-white pt-2 opacity-50" id="paragraph">
+             Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, reprehenderit voluptas id illo adipisci ullam eos porro quod neque unde? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem vero quas accusantium atque dicta est iusto dolor itaque mollitia perferendis? dolor sit, amet consectetur adipisicing elit. Tempora architecto nemo quibusdam iusto eaque qui nihil ipsa delectus, id culpa! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur culpa assumenda asperiores consequuntur ea quos libero? Exercitationem aspernatur totam nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illo consectetur unde enim omnis in provident, molestias aliquam aut ullam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam officiis quos libero dicta, asperiores aspernatur sunt cupiditate provident aperiam architecto.
+        </p>
+        <a class="pt-5 underline underline-offset-3 opacity-50">Meer Lezen?</a>
+
+    </div>
+
+  </template>
+
+<script>
+ export default {
+  name: "NewsComponent",
+}
+</script>
+
+<style scoped>
+</style>
