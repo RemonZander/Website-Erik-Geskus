4a3987725ea8f0566f345f7f2ca464105afceed0
niek221 <116267865+niek221@users.noreply.github.com>
2022-11-24 12:55:37 +0100

    Wrap around changes

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -1,25 +1,28 @@
 <template>
 
     <div class="w-1/2 float-right">
+
       <div class="width-[40%] align-right mb-[10%] pt-[10%]">
+
         <p class="text-white text-3xl align-items-end">Nieuwsoverzicht</p>
         <div class="h-2 bg-red-800"></div>
-      </div>
-      <div class="inline-flex">
-        <div class="">
-        <h2 class="text-xl" id="title">Recenste YouTube Post</h2>
-        <p class="pt-2 text-white opacity-50" id="date">19 Maart 2022</p>
-        </div>
-        <iframe class="pl-[15%]" width="420" height="315"
-        src="https://www.youtube.com/embed/tgbNymZ7vqY">
-        </iframe>
-      </div>
-      <div class="flex flex-col pt-2 text-white opacity-50" id="paragraph">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dignissimos pariatur modi maxime omnis? Dolores facere facilis nemo aperiam sapiente porro necessitatibus hic voluptatem, nesciunt temporibus sunt debitis perspiciatis molestiae?
-      <a class="pt-2 underline underline-offset-3" href="#">Meer Lezen?</a>
+
       </div>
 
-    </div>  
+      <div class="float-right w-0"></div>
+        <div class="float-right clear-right">
+          <iframe class="float-right" src="https://www.youtube.com/embed/tgbNymZ7vqY" width="420" height="315"></iframe>
+        </div>
+
+        <h2 class="text-xl" id="title">Recenste Youtube Post</h2>
+        <p class="text-white pt-2 opacity-50" id="date">19 Maart 2022</p>
+        <p class="text-white pt-2 opacity-50" id="paragraph">
+             Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, reprehenderit voluptas id illo adipisci ullam eos porro quod neque unde? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem vero quas accusantium atque dicta est iusto dolor itaque mollitia perferendis? dolor sit, amet consectetur adipisicing elit. Tempora architecto nemo quibusdam iusto eaque qui nihil ipsa delectus, id culpa! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur culpa assumenda asperiores consequuntur ea quos libero? Exercitationem aspernatur totam nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illo consectetur unde enim omnis in provident, molestias aliquam aut ullam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam officiis quos libero dicta, asperiores aspernatur sunt cupiditate provident aperiam architecto.
+        </p>
+        <a class="pt-5 underline underline-offset-3 opacity-50">Meer Lezen?</a>
+
+    </div>
+
   </template>
 
 <script>
