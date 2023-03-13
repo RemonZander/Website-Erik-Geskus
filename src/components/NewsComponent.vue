f95d48e38725f7dcba16d35861a8bf686d20835f
niek221 <116267865+niek221@users.noreply.github.com>
2023-03-13 12:51:09 +0100

    Youtube player

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -10,8 +10,8 @@
       </div>
 
       <div class="float-right w-0"></div>
-        <div class="float-right clear-right">
-          <iframe class="float-right" src="https://www.youtube.com/embed/tgbNymZ7vqY" width="420" height="315"></iframe>
+        <div class="float-right clear-right mx-5 mb-5">
+          <iframe class="float-right" src="https://www.youtube.com/embed/tgbNymZ7vqY" width="380" height="290"></iframe>
         </div>
 
         <h2 class="text-xl" id="title">Recenste Youtube Post</h2>
@@ -19,7 +19,6 @@
         <p class="text-white pt-2 opacity-50" id="paragraph">
              Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, reprehenderit voluptas id illo adipisci ullam eos porro quod neque unde? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem vero quas accusantium atque dicta est iusto dolor itaque mollitia perferendis? dolor sit, amet consectetur adipisicing elit. Tempora architecto nemo quibusdam iusto eaque qui nihil ipsa delectus, id culpa! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur culpa assumenda asperiores consequuntur ea quos libero? Exercitationem aspernatur totam nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illo consectetur unde enim omnis in provident, molestias aliquam aut ullam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam officiis quos libero dicta, asperiores aspernatur sunt cupiditate provident aperiam architecto.
         </p>
-        <a class="pt-5 underline underline-offset-3 opacity-50">Meer Lezen?</a>
 
     </div>
 
